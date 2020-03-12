9efcaf0a283936a3213ffed160a119d8fc216ff0
Rurick Maqueo Poisot <rurick@kodemia.mx>
2020-03-12 17:07:20 -0600

    Created simple api file to show diferent type of petitions using fetch or using the R&M api package

---
+++ rick_and_morty/src/lib/api.js
@@ -0,0 +1,56 @@
+const rickmortyapi = require('rickmortyapi')
+
+
+// Returns the characters count for use in the first load
+async function getCharacterCount() {
+  const response = await fetch('https://rickandmortyapi.com/api/character/')
+  const { info } = await response.json()
+  return info.count
+}
+
+// Returns one character data for use in individual components
+async function getCharacterById(id) {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+  return response.json()
+}
+
+// returns all characters iterative
+async function getAllCharacters() {
+  const { info, results } = await rickmortyapi.getCharacter()
+  let characters = [
+    ...results
+  ]
+
+  for (let i = 2; i <= info.pages; i++) {
+    console.log('Voy en', i)
+    const { results } = await rickmortyapi.getCharacter({ page: i })
+    characters = [
+      ...characters,
+      ...results
+    ]
+  }
+
+  return characters
+}
+
+// returns all characters recursively
+async function getCharactersFromPage(pageNumber = 1) {
+  let characters = []
+  const { info, results } = await rickmortyapi.getCharacter({ page: pageNumber })
+
+  if (info.next !== '') {
+    characters = await getCharactersFromPage(pageNumber + 1)
+  }
+
+  return [
+    ...characters,
+    ...results
+  ]
+}
+
+module.exports = {
+  getCharacterCount,
+  getCharacterById,
+  getAllCharacters,
+  getCharactersFromPage
+}
