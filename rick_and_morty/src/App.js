320199e1a273fd4496eaa8b2da3248b30cd234c4
Rurick Maqueo Poisot <rurick@kodemia.mx>
2020-03-12 17:08:11 -0600

    Converted the funciton compotent to a class component and implement the api file created to get the characters count

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-container">WIP</div>
-    </div>
-  );
+import api from './lib/api'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    api.getCharacterCount()
+      .then(count => {
+        this.setState({
+          count
+        })
+      })
+      .catch(error => console.log(error))
+
+    api.getCharacterById(1)
+      .then(character => console.log(character))
+      .catch()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-container">WIP / Character count {this.state.count}</div>
+      </div>
+    )
+  }
 }
 
 export default App;
