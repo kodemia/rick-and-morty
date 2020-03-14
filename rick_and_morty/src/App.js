1794ce5063eda953206c147933d9e29dad109ff0
DavidMoranchel <davidm@apli.jobs>
2020-03-13 20:08:10 -0600

    list works

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -1,36 +1,57 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import api from './lib/api'
+import api from "./lib/api";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      count: 0
-    }
+      count: 0,
+      characters: []
+    };
   }
 
   componentDidMount() {
-    api.getCharacterCount()
-      .then(count => {
-        this.setState({
-          count
-        })
-      })
-      .catch(error => console.log(error))
-
-    api.getCharacterById(1)
-      .then(character => console.log(character))
-      .catch()
+    api.getAllCharacters().then(({ results }) => {
+      const characters = results.map(({ id, name, image }) => {
+        return { id, name, image };
+      });
+      this.setState({
+        characters: characters
+      });
+    });
   }
 
+  _renderCards = ({ id, name, image }) => {
+    return (
+      <div key={id} id={id} className="Card">
+        <div className="Card-image">
+          <figure>
+            <img src={image} alt={name} />
+          </figure>
+        </div>
+        <div className="Card-description">
+          <div className="Card-name">
+            <h3>{name}</h3>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   render() {
+    const { characters } = this.state;
     return (
       <div className="App">
-        <div className="App-container">WIP / Character count {this.state.count}</div>
+        <div className="App-container">
+          <h1>Rick and morty characters {this.state.count}</h1>
+          <div className="Cards-container">
+            {characters.map(c => this._renderCards(c))}
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
--- rick_and_morty/src/lib/api.js
+++ rick_and_morty/src/lib/api.js
@@ -1,56 +1,60 @@
-const rickmortyapi = require('rickmortyapi')
-
-
 // Returns the characters count for use in the first load
 async function getCharacterCount() {
-  const response = await fetch('https://rickandmortyapi.com/api/character/')
-  const { info } = await response.json()
-  return info.count
+  const response = await fetch("https://rickandmortyapi.com/api/character/");
+  const { info } = await response.json();
+  return info.count;
 }
 
 // Returns one character data for use in individual components
 async function getCharacterById(id) {
-  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
-  return response.json()
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/${id}`
+  );
+  return response.json();
+}
+
+async function getAllCharacters() {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/`);
+  return response.json();
 }
 
 // returns all characters iterative
-async function getAllCharacters() {
-  const { info, results } = await rickmortyapi.getCharacter()
-  let characters = [
-    ...results
-  ]
+// async function getAllCharacters() {
+//   const { info, results } = await rickmortyapi.getCharacter()
+//   let characters = [
+//     ...results
+//   ]
 
-  for (let i = 2; i <= info.pages; i++) {
-    console.log('Voy en', i)
-    const { results } = await rickmortyapi.getCharacter({ page: i })
-    characters = [
-      ...characters,
-      ...results
-    ]
-  }
+//   for (let i = 2; i <= info.pages; i++) {
+//     console.log('Voy en', i)
+//     const { results } = await rickmortyapi.getCharacter({ page: i })
+//     characters = [
+//       ...characters,
+//       ...results
+//     ]
+//   }
 
-  return characters
-}
+//   return characters
+// }
 
-// returns all characters recursively
-async function getCharactersFromPage(pageNumber = 1) {
-  let characters = []
-  const { info, results } = await rickmortyapi.getCharacter({ page: pageNumber })
+// // returns all characters recursively
+// async function getCharactersFromPage(pageNumber = 1) {
+//   let characters = []
+//   const { info, results } = await rickmortyapi.getCharacter({ page: pageNumber })
 
-  if (info.next !== '') {
-    characters = await getCharactersFromPage(pageNumber + 1)
-  }
+//   if (info.next !== '') {
+//     characters = await getCharactersFromPage(pageNumber + 1)
+//   }
 
-  return [
-    ...characters,
-    ...results
-  ]
-}
+//   return [
+//     ...characters,
+//     ...results
+//   ]
+// }
 
 module.exports = {
   getCharacterCount,
   getCharacterById,
-  getAllCharacters,
-  getCharactersFromPage
-}
+  getAllCharacters
+  // getCharactersFromPage
+};
