a5eb4bc4beafd4f1c8f9c9141429d94f5d852e11
DavidMoranchel <davidm@apli.jobs>
2020-03-13 21:52:33 -0600

    v0.0.1

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 
 import api from "./lib/api";
+import pickle from "./images/pickle-rick.png";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
-      characters: []
+      characters: [],
+      activeCharacter: {},
+      modalActive: false
     };
   }
 
@@ -23,9 +26,31 @@ class App extends Component {
     });
   }
 
+  _setActiveCharacter(id) {
+    api.getCharacterById(id).then(data => {
+      console.log(data);
+      this.setState({
+        activeCharacter: data,
+        modalActive: true
+      });
+    });
+  }
+
+  _unsetActiveCharacter() {
+    this.setState({
+      activeCharacter: {},
+      modalActive: false
+    });
+  }
+
   _renderCards = ({ id, name, image }) => {
     return (
-      <div key={id} id={id} className="Card">
+      <div
+        key={id}
+        id={id}
+        className="Card"
+        onClick={e => this._setActiveCharacter(id)}
+      >
         <div className="Card-image">
           <figure>
             <img src={image} alt={name} />
@@ -41,7 +66,7 @@ class App extends Component {
   };
 
   render() {
-    const { characters } = this.state;
+    const { characters, modalActive, activeCharacter } = this.state;
     return (
       <div className="App">
         <div className="App-container">
@@ -50,6 +75,53 @@ class App extends Component {
             {characters.map(c => this._renderCards(c))}
           </div>
         </div>
+
+        {modalActive ? (
+          <div className="Modal" onClick={e => this._unsetActiveCharacter()}>
+            <div className="Pickle-container">
+              <figure>
+                <img src={pickle} alt="pickle-rick" />
+              </figure>
+            </div>
+
+            <div className="Card-detail">
+              <div className="Card-image">
+                <figure>
+                  <img src={activeCharacter.image} alt={activeCharacter.name} />
+                </figure>
+              </div>
+              <div className="Card-detail-description">
+                <div className="Card-name">
+                  <h3>{activeCharacter.name}</h3>
+                  <div className="Characteristic">
+                    <p>Status</p>
+                    <p className="Characteristic-value">
+                      {activeCharacter.status}
+                    </p>
+                  </div>
+                  <div className="Characteristic">
+                    <p>Specie</p>
+                    <p className="Characteristic-value">
+                      {activeCharacter.species}
+                    </p>
+                  </div>
+                  <div className="Characteristic">
+                    <p>Gender</p>
+                    <p className="Characteristic-value">
+                      {activeCharacter.gender}
+                    </p>
+                  </div>
+                  <div className="Characteristic">
+                    <p>Origin</p>
+                    <p className="Characteristic-value">
+                      {activeCharacter.origin.name}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
     );
   }
